e20dde153b935609a522e420da3a872258a3b507
Dave Anscombe <dave.anski@gmail.com>
2024-11-20 16:21:45 +0000

    Create superuser and register models in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Author, Category, Tag, Comment
+
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
