b591afb86c717682546cc83ca0a42931e6d07bee
Dave Anscombe <dave.anski@gmail.com>
2024-11-21 19:28:00 +0000

    Add basic prepoulation of slug field for posts

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Post, Author, Category, Tag, Comment
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Tag)
