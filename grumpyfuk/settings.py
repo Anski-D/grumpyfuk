0236060a0b031792ec02db36351e0bcdc2473ac5
Dave Anscombe <dave.anski@gmail.com>
2024-11-22 16:06:08 +0000

    Hide secret key for deployment

--- grumpyfuk/settings.py
+++ grumpyfuk/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0^%wbr0$$1%7n*+h#@pf4jak&tfsmo_#pv%z+e*7zww9su$)rn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0^%wbr0$$1%7n*+h#@pf4jak&tfsmo_#pv%z+e*7zww9su$)rn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
