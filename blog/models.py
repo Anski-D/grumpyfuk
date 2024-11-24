1e58aad97577b5c791f2a1d6305322973de4c22b
Dave Anscombe <dave.anski@gmail.com>
2024-11-24 20:01:43 +0000

    Add published boolean to Post model

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     tags = models.ManyToManyField('Tag', related_name='posts')
     author = models.ForeignKey('Author', related_name='posts', on_delete=models.SET_NULL, null=True)
     slug = models.SlugField(max_length=36, unique=True)
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
