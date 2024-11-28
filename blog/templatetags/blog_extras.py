1d0d1f8ead98e056fdea1ce7b81782630dd67d53
Dave Anscombe <dave.anski@gmail.com>
2024-11-28 20:36:54 +0000

    Create template filter for applying markdown
    * For posts, it applies full markdown.
    * For lists of posts, the paragraph tags are stripped so that only one line is rendered, but other markdown gets applied.

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,22 @@
+import markdown
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def render_markdown(value):
+    md = markdown.Markdown(extensions=['fenced_code'])
+
+    return mark_safe(md.convert(value))
+
+
+@register.filter
+@stringfilter
+def render_markdown_no_paras(value):
+    md = markdown.Markdown(extensions=['fenced_code'])
+
+    return mark_safe(md.convert(value).replace('<p>', ' ').replace('</p>', ''))
