f84c2abf2b0940ab26f2c963ac42db9a3127025b
Dave Anscombe <dave.anski@gmail.com>
2024-12-01 18:07:42 +0000

    Add Markdown extension for converting slugs to internal site links

---
+++ blog/markdown_extensions.py
@@ -0,0 +1,21 @@
+import markdown
+from django.urls import reverse
+from markdown.inlinepatterns import LinkInlineProcessor, LINK_RE
+
+
+class SlugFieldLinkInlineProcessor(LinkInlineProcessor):
+    def getLink(self, data, index):
+        href, title, index, handled = super().getLink(data, index)
+        if href.startswith('slug'):
+            _, app, name, slug = href.split(':')
+            relative_url = reverse(f'{app}:{name}', args=[slug])
+            href = relative_url
+
+        return href, title, index, handled
+
+
+class SlugFieldExtensions(markdown.Extension):
+    def extendMarkdown(self, md, *args, **kwargs):
+        md.inlinePatterns.register(
+            SlugFieldLinkInlineProcessor(LINK_RE, md), 'link', 160
+        )
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,12 +3,14 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
+from blog.markdown_extensions import SlugFieldExtensions
+
 register = template.Library()
 
 
 @register.filter
 @stringfilter
 def render_markdown(value):
-    md = markdown.Markdown(extensions=['fenced_code'])
+    md = markdown.Markdown(extensions=['fenced_code', SlugFieldExtensions()])
 
     return mark_safe(md.convert(value))
