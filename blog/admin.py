d09ccc1d3120cf89218602fa9ca483f86970e2f0
Dave Anscombe <dave.anski@gmail.com>
2024-12-03 19:43:02 +0000

    Add post counts to blog admin and inherit common code between admin classes where possible

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import Post, Author, Category, Tag, Comment
+
+
+class CommonAdmin(admin.ModelAdmin):
+    @admin.display(description="Number of posts")
+    def count_posts(self, obj):
+        return obj.posts__count
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(Count('posts'))
+
+        return queryset
 
 
 @admin.register(Post)
@@ -9,17 +22,21 @@ class PostAdmin(admin.ModelAdmin):
 
 
 @admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(CommonAdmin):
+    list_display = ('display_name', 'count_posts')
     prepopulated_fields = {"slug": ("first_name", "last_name")}
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+class CategoryAdmin(CommonAdmin):
+    list_display = ('name', 'count_posts')
+    prepopulated_fields = {'slug': ('name',)}
+
 
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(CommonAdmin):
+    list_display = ('name', 'count_posts')
     prepopulated_fields = {"slug": ("name",)}
 
 
