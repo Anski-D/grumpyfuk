b70569b60b50454b440ec7c6cf4c60c061d6bd97
Dave Anscombe <dave.anski@gmail.com>
2024-12-17 20:57:39 +0000

    Make sure Django only serves media files when DEBUG is on, otherwise get host to serve directly

--- grumpyfuk/urls.py
+++ grumpyfuk/urls.py
@@ -36,7 +36,8 @@ urlpatterns += [
 ]
 
 # Media URLs
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # URLs for sitemaps
 info_dict = {
